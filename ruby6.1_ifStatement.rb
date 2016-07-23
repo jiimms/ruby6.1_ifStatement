948097b974f4c83d0b728c89bac4d14946d4c9a7
jiimms <jiimms@yahoo.com>
2016-07-23 19:54:41 +0530

    modified

--- ruby6.1_ifStatement.rb
+++ ruby6.1_ifStatement.rb
@@ -1,7 +1,8 @@
 class Result
 	def result
+		student_result = ""
 		puts "Please enter name"
-		name = gets
+		name = gets.chomp
 
 		puts "Please enter marks1"
 		marks1 = gets.to_i
@@ -12,21 +13,20 @@ class Result
 		puts "Please enter marks3"
 		marks3 = gets.to_i
 
+		if marks1 <40 || marks2<40 || marks3 <40
+			student_result = "fail"
+		else
+			student_result = "pass"
+		end
 		total = marks1 + marks2+ marks3
 
-		#total >=40 ? (result = "pass"):(result = "fail")
-		if total >= 40
-			result = "pass"
-		else
-			result = "fail"
-		end
 
 		puts "Name: #{name}"
 		puts "Marks1: #{marks1}"
 		puts "Marks2: #{marks2}"
 		puts "Marks3: #{marks3}"
 		puts "Total: #{total}"
-		puts "Result: #{result}"
+		puts "Result: #{student_result}"
 		
 	end
 end
